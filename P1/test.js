b8fb857cfe383169a494e8e278d86ca9794d3576
mohammadreza-amini <mr.amini.env@gmail.com>
2025-02-18 18:34:50 +0330

    part 3

--- P1/test.js
+++ P1/test.js
@@ -1,2 +1,16 @@
-let title = document.querySelector("#title").textContent;
-console.log(title);
+let ranNumber = Math.floor(Math.random() * 100);
+document.querySelector("#buttonArea").addEventListener('click', () => {
+    let userGuess = Number(document.querySelector("#userGuess").value);
+    let showResut = document.querySelector('.test');
+    if (userGuess && userGuess >= 1) {
+        if (userGuess > ranNumber) {
+            showResut.innerHTML = "too big";
+        } else if (userGuess < ranNumber) {
+            showResut.innerHTML = "too short";
+        } else {
+            showResut.innerHTML = "you win!";
+            alert("you win!");
+        }
+    }
+});
+
