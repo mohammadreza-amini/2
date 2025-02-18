96c1dea2146c22048a2428f63b3277bd319af7ce
mohammadreza-amini <mr.amini.env@gmail.com>
2025-02-18 16:37:08 +0330

    part 2

--- P1/test.js
+++ P1/test.js
@@ -1,11 +1,2 @@
-function averageNumber(num1, num2){
-    return(num1 + num2)/2
-}
-
-function result(numberOne , numberTwo) {
-    const avg = averageNumber(numberOne , numberTwo);
-    const txt = `your average is ${avg}`;
-    return txt;
-}
-
-console.log(result(10 , 14));
+let title = document.querySelector("#title").textContent;
+console.log(title);
