354fc69b79b778113066d6be980fec27952b77fb
mohammadreza-amini <mr.amini.env@gmail.com>
2025-02-24 17:01:02 +0330

    part 4

--- number guessing game/test.js
+++ number guessing game/test.js
@@ -8,7 +8,9 @@ document.querySelector("#buttonArea").addEventListener('click', () => {
         } else if (userGuess < ranNumber) {
             showResut.innerHTML = "too short";
         } else {
-            document.querySelector('.background').style.backgroundColor ="rgba(66, 226, 66, 0.9)";
+            document.querySelector('.background').style.backgroundColor = "rgba(66, 226, 66, 0.9)";
+            showResut.style.color = 'red';
+            document.querySelector("#userGuess").disabled = true;
             showResut.innerHTML = "you win!";
         }
     }
